ce1169f1ea6249a00c6216a44c89a88b19f5b3c2
kowshikdontu <164200412+kowshikdontu@users.noreply.github.com>
2025-06-24 01:20:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://backtracker-b163.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
